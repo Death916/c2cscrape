872686e9d55789986f685503695a3622aafda7e6
death916 <mail@trentnelson.dev>
2025-12-24 10:00:38 -0800

    dont save in individual folders

--- src/c2cscrape.py
+++ src/c2cscrape.py
@@ -94,6 +94,8 @@ class Qbittorrent:
         self.password = ""
         self.host = "localhost"
         self.port = 8080
+        self.content_layout = "NoSubfolder"
+
 
     def get_credentials(self):
         """Get qbittorrent credentials from .env file"""
@@ -132,7 +134,7 @@ class Qbittorrent:
 
         for link in links:
             try:
-                torrent.torrents_add(urls=link, save_path=self.download_path)
+                torrent.torrents_add(urls=link, save_path=self.download_path, content_layout=self.content_layout)
                 logging.info(f"Added torrent {link}  to qbittorrent")
             except Exception as e:
                 logging.error(f"Error adding torrent {link} to qbittorrent: {e}")
